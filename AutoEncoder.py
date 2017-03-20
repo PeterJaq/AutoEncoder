026de83f5cd25d727c3f1cb2120f9c12e299c5a1
JIANG ANQING <anqingjiang0524@akane.waseda.jp>
2017-03-20 14:40:13 +0800

    commit

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -108,7 +108,7 @@ for epoch in range(training_epochs):
     if epoch % display_step == 0:
         print("Epoch:", '%04d' % (epoch + 1), "cost = ",
               "{:.9f}".format(avg_cost))
-        
+
 print("Total cost: " + str(autoencoder.calc_total_cost(X_test)))
 
 
